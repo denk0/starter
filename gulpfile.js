668ed1741a295be2c664b22b572585f276e54ca2
Vladyslav Romanenko <denko1024@gmail.com>
2017-01-10 17:44:30 +0200

    Remove jshint from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -31,8 +31,8 @@ gulp.task('styles', function() {
 // Scripts
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
+    //.pipe(jshint('.jshintrc'))
+    //.pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/scripts'))
     .pipe(rename({ suffix: '.min' }))
